933fd31c48a8da00c9cfd9ca0ae74c09ea4033a1
khfiii <khfii635@gmail.com>
2024-05-21 08:48:04 -0400

    First Setup

---
+++ database/migrations/2024_05_21_124532_create_pesertas_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pesertas', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pesertas');
+    }
+};
