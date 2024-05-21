8f538ddb4f7d3456cc60dfcd0ad850d48d2322a2
khfiii <khfii635@gmail.com>
2024-05-21 12:18:39 -0400

    Add peserta resource

---
+++ app/Filament/Resources/PesertaResource.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\PesertaResource\Pages;
+use App\Filament\Resources\PesertaResource\RelationManagers;
+use App\Models\Peserta;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class PesertaResource extends Resource
+{
+    protected static ?string $model = Peserta::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPesertas::route('/'),
+            'create' => Pages\CreatePeserta::route('/create'),
+            'edit' => Pages\EditPeserta::route('/{record}/edit'),
+        ];
+    }
+}
