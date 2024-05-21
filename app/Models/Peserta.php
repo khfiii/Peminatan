77b388b5370cfcb1267f92363f8d8d6adf082e2e
khfiii <khfii635@gmail.com>
2024-05-21 12:17:48 -0400

    defining relationship in model peserta

--- app/Models/Peserta.php
+++ app/Models/Peserta.php
@@ -2,10 +2,34 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Jawaban;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Peserta extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'nama', 
+        'tanggal_lahir', 
+        'phone', 
+        'email', 
+        'sekolah_asal', 
+        'tahun_lulus'
+    ]; 
+
+    public function jawaban(): HasOne
+    {
+        return $this->hasOne(Jawaban::class); 
+    }
+
+    public function nilai(): BelongsToMany
+    {
+        return $this->belongsToMany(Soal::class); 
+    }
+
+
 }
