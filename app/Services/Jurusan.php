e3ef4ba6429e11efa3c2dae177dd8676c29695a6
khfiii <khfii635@gmail.com>
2024-05-23 13:33:58 -0400

    Configure Jurusan Service

---
+++ app/Services/Jurusan.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Services;
+
+
+class Jurusan 
+{
+    public function generateCode(?string $string) : string
+    {
+
+        $words = explode(' ', $string);
+
+        $word = ''; 
+
+        foreach($words as $result){
+                $word .= substr($result, 0, 1);
+        }
+
+        return strtoupper($word); 
+    } 
+}
