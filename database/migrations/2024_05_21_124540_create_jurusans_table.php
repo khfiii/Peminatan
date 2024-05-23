f3f47ddff49f8b0016e6a623f7bcb49560087fb0
khfiii <khfii635@gmail.com>
2024-05-23 07:41:10 -0400

    Setup Structure Migration Jurusan

--- database/migrations/2024_05_21_124540_create_jurusans_table.php
+++ database/migrations/2024_05_21_124540_create_jurusans_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('jurusans', function (Blueprint $table) {
             $table->id();
+            $table->string('kode_jurusan'); 
+            $table->text('nama_jurusan');
             $table->timestamps();
         });
     }
