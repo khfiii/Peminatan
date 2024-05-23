65fc1b5864a20cce538fa33639306adf92ba185b
khfiii <khfii635@gmail.com>
2024-05-23 07:43:53 -0400

    Setup relationship in jurusan model

--- app/Models/Jurusan.php
+++ app/Models/Jurusan.php
@@ -2,12 +2,19 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Soal;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Jurusan extends Model
 {
     use HasFactory;
 
     protected $fillable = ['kode_jurusan', 'nama_jurusan']; 
+
+    public function soals(): BelongsToMany
+    {
+        return $this->belongsToMany(Soal::class);
+    }
 }
