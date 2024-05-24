8ddd47cba6b9dc7841c22497981c3f934d9cb04b
khfiii <khfii635@gmail.com>
2024-05-24 00:32:38 -0400

    Setup icon for soal

--- app/Filament/Resources/JurusanResource.php
+++ app/Filament/Resources/JurusanResource.php
@@ -52,6 +52,7 @@ class JurusanResource extends Resource
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
