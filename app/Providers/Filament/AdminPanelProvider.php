36df35917d367faa335163d8c9fcd55ce4e4f3b6
khfiii <khfii635@gmail.com>
2024-05-25 22:33:15 +0800

    Using daisyui component

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -53,6 +53,7 @@ class AdminPanelProvider extends PanelProvider
             ->authMiddleware([
                 Authenticate::class,
             ])
+            ->spa()
             ->viteTheme('resources/css/filament/admin/theme.css');
     }
 }
