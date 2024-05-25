9c43c8ccc479e803291ead04c6a2ba0bb4a36a49
khfiii <khfii635@gmail.com>
2024-05-25 09:27:07 -0400

    Fix layout

--- resources/views/components/empty-state.blade.php
+++ resources/views/components/empty-state.blade.php
@@ -1,7 +1,7 @@
 <div class="text-center">
           <p class="text-base font-semibold text-purple-700">Datanya Masih Kosong</p>
           <p class="mt-6 text-base leading-7 text-gray-600">Soal peminatan ini belum punya pertanyaan 🙂</p>
-          <img src="{{ Vite::asset('resources/images/empty.svg') }}" class="mx-auto w-[10rem]" alt="">
+          <img src="{{ Vite::asset('resources/images/empty.svg') }}" class="mx-auto" width="200px" alt="">
           <div class="mt-10 flex items-center justify-center gap-x-6">
             <a href="https://wa.link/dp9hww" target="_blank" class="text-sm font-semibold bg-purple-700 p-2 rounded-md text-gray-900">Butuh Bantuan <span aria-hidden="true">&rarr;</span></a>
           </div>
